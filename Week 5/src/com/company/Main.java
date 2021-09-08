08f516765a60ee9043abb58794d30ebf4fecf4e7
Daniel Vazquez <dvazquez21@berkeley.edu>
2021-09-08 08:25:20 -0700

    Java Loop Map

--- Week 5/src/com/company/Main.java
+++ Week 5/src/com/company/Main.java
@@ -1,27 +1,27 @@
-package com.company;
-
-import java.sql.SQLOutput;
-import java.util.Scanner;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// invalid names
-//        int 4uber;
-//        int Test ing;
-//        String if;
-
-        int test1;
-        String test2;
-        System.out.println(test1);
-        System.out.println(test2);
-
-        int outer = 1;
-        if(outer == 1){
-            int inner = 0;
-        }
-        System.out.println(inner);
-
-
-    }
-}
+//package com.company;
+//
+//import java.sql.SQLOutput;
+//import java.util.Scanner;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//	// invalid names
+////        int 4uber;
+////        int Test ing;
+////        String if;
+//
+//        int test1;
+//        String test2;
+//        System.out.println(test1);
+//        System.out.println(test2);
+//
+//        int outer = 1;
+//        if(outer == 1){
+//            int inner = 0;
+//        }
+//        System.out.println(inner);
+//
+//
+//    }
+//}
